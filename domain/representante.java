a1fd99ad3ff010a842ced11361f5f8a1b4ea4c2c
Andres Burcheri <andresvoet10@gmail.com>
2026-05-19 12:48:46 -0300

    Agrego clase Representante

---
+++ domain/representante.java
@@ -0,0 +1,23 @@
+public class Representante{
+    private String email;
+    private PersonaJuridica personaJuridica;
+    private EntidadBeneficiaria entidadBeneficiaria;
+
+    public Representante(String email, PersonaJuridica personaJuridica, EntidadBeneficiaria entidadBeneficiaria) {
+        this.email = email;
+        this.personaJuridica = personaJuridica;
+        this.entidadBeneficiaria = entidadBeneficiaria;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public PersonaJuridica getPersonaJuridica() {
+        return personaJuridica;
+    }
+
+    public EntidadBeneficiaria getEntidadBeneficiaria() {
+        return entidadBeneficiaria;
+    }
+}
