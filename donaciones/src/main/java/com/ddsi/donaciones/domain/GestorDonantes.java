8f09e061eb65459201957532f70e592e6fce9eea
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-05-19 14:06:20 -0300

    Completa GestorDonante

--- donaciones/src/main/java/com/ddsi/donaciones/domain/GestorDonantes.java
+++ donaciones/src/main/java/com/ddsi/donaciones/domain/GestorDonantes.java
@@ -1,9 +1,16 @@
 package com.ddsi.donaciones.domain;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class GestorDonantes {
-    public static GestorDonantes gestorDonantes = null;
+    private static GestorDonantes gestorDonantes = null;
+    private ArrayList<Donante> donantesRegistrados = null;
 
     private GestorDonantes() {
+        donantesRegistrados = new ArrayList<>();
     }
 
     public static GestorDonantes getInstance() {
@@ -12,5 +19,51 @@ public class GestorDonantes {
         }
 
         return gestorDonantes;
+    }
+
+    public void registrarDonante(Donante donante) {
+        donantesRegistrados.add(donante);
+    }
+
+    // import com.ddsi.donaciones.domain.GestorDonantes;
+    //
+    // try {
+    //     GestorDonantes.getInstance().cargarCSV("/home/leonardo/archivo.csv");
+    // } catch (FileNotFoundException e) {
+    //     System.out.println("Que no panda el cúnico");
+    // }
+
+    public void cargarCSV(String path) throws FileNotFoundException {
+        File csv = new File(path);
+        Scanner scannerRegistros = new Scanner(csv);
+
+        // HEADER
+        String header = scannerRegistros.nextLine();
+        Scanner scannerHeader = new Scanner(header);
+        scannerHeader.useDelimiter(", ");
+
+        System.out.println("Header: " + header);
+        while (scannerHeader.hasNext()) {
+            String campo = scannerHeader.next();
+            System.out.println("\t- Header Campo: " + campo);
+        }
+        scannerHeader.close();
+
+        // DATOS
+        while (scannerRegistros.hasNextLine()) {
+            String registro = scannerRegistros.nextLine();
+            Scanner scannerRegistro = new Scanner(registro);
+            scannerRegistro.useDelimiter(", ");
+
+            System.out.println("Registro: " + registro);
+            while (scannerRegistro.hasNext()) {
+                String campo = scannerRegistro.next();
+                System.out.println("\t- Campo: " + campo);
+            }
+
+            scannerRegistro.close();
+        }
+
+        scannerRegistros.close();
     }
 }
