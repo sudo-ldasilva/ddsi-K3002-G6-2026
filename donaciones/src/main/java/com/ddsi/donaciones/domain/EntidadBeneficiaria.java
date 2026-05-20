c0ad5dedfc34ab87d514f262856a2cb904aa0aa6
marcosegan <megan@frba.utn.edu.ar>
2026-05-20 11:10:12 -0300

    Agrego EntidadBeneficiaria

---
+++ donaciones/src/main/java/com/ddsi/donaciones/domain/EntidadBeneficiaria.java
@@ -0,0 +1,37 @@
+package com.ddsi.donaciones.domain;
+
+import java.util.ArrayList;
+
+public class EntidadBeneficiaria {
+
+    private String razonSocial;
+    private String tipo;
+
+    // ContactoTelefonoFijo / ContactoCelular?
+    private String telefono;
+
+    private Direccion direccion;
+
+    // ArrayList, LinkedList u otro?
+    private ArrayList<Representante> representantes;
+    private ArrayList<Necesidad> necesidades;
+
+    public EntidadBeneficiaria(
+            String razonSocial, String tipo, String telefono, Direccion direccion)
+    {
+        this.razonSocial = razonSocial;
+        this.tipo = tipo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.representantes = new ArrayList<Representante>();
+        this.necesidades = new ArrayList<Necesidad>();
+    }
+
+    public void añadirRepresentante(Representante representante) {
+        representantes.add(representante);
+    }
+
+    public void quitarRepresentante(Representante representante) {
+        representantes.remove(representante);
+    }
+}
