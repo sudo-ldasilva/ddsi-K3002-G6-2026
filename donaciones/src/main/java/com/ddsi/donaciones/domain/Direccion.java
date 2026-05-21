21cbfc154e75fc26a5fd88b02f0d13e9657e3db0
marcosegan <megan@frba.utn.edu.ar>
2026-05-21 18:44:14 -0300

    feat(Direccion): agrego String nroDepartamento

--- donaciones/src/main/java/com/ddsi/donaciones/domain/Direccion.java
+++ donaciones/src/main/java/com/ddsi/donaciones/domain/Direccion.java
@@ -5,22 +5,30 @@ public class Direccion {
     private String calleEntre1;
     private String calleEntre2;
     private String altura;
+    private String nroDepartamento;
     private Ciudad ciudad;
 
-    public Direccion(String callePrincipal, String calleEntre1, String calleEntre2, String altura, Ciudad ciudad) {
+    public Direccion(String callePrincipal, String calleEntre1, String calleEntre2, String altura, String nroDepartamento, Ciudad ciudad) {
         this.callePrincipal = callePrincipal;
         this.calleEntre1 = calleEntre1;
         this.calleEntre2 = calleEntre2;
         this.altura = altura;
+        this.nroDepartamento = nroDepartamento;
         this.ciudad = ciudad;
     }
 
     public String direccion(){
-        if (altura != null) {
-            return this.callePrincipal + " " + this.altura;
-        }
+        String direccion = this.callePrincipal;
 
-        return this.callePrincipal + " (Entre " + this.calleEntre1 + " y " + this.calleEntre2 + ")";
+        if (altura != null)
+            direccion += " " + this.altura;
+        else
+            direccion += " (Entre " + this.calleEntre1 + " y " + this.calleEntre2 + ")";
+
+        if (nroDepartamento != null)
+            direccion += " (Departamento " + nroDepartamento + ")";
+
+        return direccion;
     }
 
     public String direccionCompleta(){
