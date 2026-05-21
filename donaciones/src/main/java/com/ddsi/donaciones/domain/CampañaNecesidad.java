447980bf9562beed8659a6ee5439660e5ff033fc
BelRicci <bricciardi@frba.utn.edu.ar>
2026-05-21 15:20:32 -0300

    Fix ñ en CampaniaNecesidad

--- donaciones/src/main/java/com/ddsi/donaciones/domain/CampañaNecesidad.java
+++ donaciones/src/main/java/com/ddsi/donaciones/domain/CampañaNecesidad.java
@@ -2,14 +2,14 @@ package com.ddsi.donaciones.domain;
 
 import java.util.ArrayList;
 
-public class CampañaNecesidad {
+public class CampaniaNecesidad {
     ArrayList<NecesidadIndividual> necesidades;
     EntidadBeneficiaria entidadBeneficiaria;
     String descripcion;
     String estado;
     Subcategoria subcategoria;
 
-    public CampañaNecesidad(EntidadBeneficiaria entidadBeneficiaria, String descripcion, String estado, Subcategoria subcategoria) {
+    public CampaniaNecesidad(EntidadBeneficiaria entidadBeneficiaria, String descripcion, String estado, Subcategoria subcategoria) {
         this.necesidades = new ArrayList<>();
         this.entidadBeneficiaria = entidadBeneficiaria;
         this.descripcion = descripcion;
