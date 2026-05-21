da491fe4b384f1181be4ab3212ab9466b70172b3
BelRicci <bricciardi@frba.utn.edu.ar>
2026-05-21 15:22:46 -0300

    Fix ñ en CampaniaNecesidadExtraoridania

--- donaciones/src/main/java/com/ddsi/donaciones/domain/CampañaNecesidadExtraordinaria.java
+++ donaciones/src/main/java/com/ddsi/donaciones/domain/CampañaNecesidadExtraordinaria.java
@@ -1,9 +1,9 @@
 package com.ddsi.donaciones.domain;
 
-public class CampañaNecesidadExtraordinaria extends CampañaNecesidad {
+public class CampaniaNecesidadExtraordinaria extends CampaniaNecesidad {
     private String situacionExcepcional;
 
-    public CampañaNecesidadExtraordinaria(EntidadBeneficiaria entidadBeneficiaria, String descripcion, String estado, Subcategoria subcategoria, String situacionExcepcional) {
+    public CampaniaNecesidadExtraordinaria(EntidadBeneficiaria entidadBeneficiaria, String descripcion, String estado, Subcategoria subcategoria, String situacionExcepcional) {
         super(entidadBeneficiaria, descripcion, estado, subcategoria);
         this.situacionExcepcional = situacionExcepcional;
     }
