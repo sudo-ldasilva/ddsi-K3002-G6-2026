8ec81ff270c2c361ddb1920da5d2330034693b26
BelRicci <bricciardi@frba.utn.edu.ar>
2026-05-21 16:51:10 -0300

    Update ContactoTelefono.java

--- donaciones/src/main/java/com/ddsi/donaciones/domain/ContactoTelefono.java
+++ donaciones/src/main/java/com/ddsi/donaciones/domain/ContactoTelefono.java
@@ -1,13 +1,18 @@
 package com.ddsi.donaciones.domain;
-
+//Vamos a mandar el string completo, de ahi se separa el codigo de area y si es fijo
 public class ContactoTelefono implements Contacto {
     private String numero;
     private String codigoDeArea;
     private boolean esFijo;
 
-    public ContactoTelefono(String codigoDeArea, String num, boolean fijo) {
-        this.numero = this.codigoDeArea + numero;
-        this.esFijo = fijo;
+    public ContactoTelefono(String numeroCompleto) {
+        String[] partesDelNumero = numeroCompleto.split(" ",2);
+        this.codigoDeArea = partesDelNumero[0];
+        this.numero = partesDelNumero[1];
+        if(this.numero.startsWith("11"){
+            this.esFijo = false;
+        }
+        else{ this.esFijo = true; }
     }
 
     public void MandarMensaje(String mensaje) {
