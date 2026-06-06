0434a30709ce776b8757e84257f75be9d9b37dac
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-06-06 20:12:10 -0300

    Agrega servicio de incentivos

---
+++ incentivos/src/main/java/com/ddsi/incentivos/IncentivosApplication.java
@@ -0,0 +1,13 @@
+package com.ddsi.incentivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncentivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncentivosApplication.class, args);
+	}
+
+}
---
+++ incentivos/src/test/java/com/ddsi/incentivos/IncentivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddsi.incentivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncentivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
