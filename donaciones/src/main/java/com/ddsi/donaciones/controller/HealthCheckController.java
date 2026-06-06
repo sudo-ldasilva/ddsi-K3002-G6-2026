2d1f829cc91a64b33574036dac474710cb34584a
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-06-06 20:26:01 -0300

    Agrega health check a donaciones

---
+++ donaciones/src/main/java/com/ddsi/donaciones/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.ddsi.donaciones.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	  @GetMapping("/health")
+	  public String index() {
+		  return "Health Check OK!";
+	  }
+
+}
