1b2a88004a0a46bc55a5802d2fe16fe683bf6a13
miluuy <mtietirey@frba.utn.edu.ar>
2026-06-17 19:09:43 -0300

    borro lo viejo, así está limpio para implementar las APIs

--- notificaciones/src/main/java/com/ddsi/notificaciones/domain/ContactoSMS.java
+++ notificaciones/src/main/java/com/ddsi/notificaciones/domain/ContactoSMS.java
@@ -1,16 +1,9 @@
 package com.ddsi.notificaciones;
 
 public class ContactoSMS extends ContactoTelefono {
-    private String usuario;
-
-    public ContactoSMS(String numeroCompleto, String usuario) {
-        super(numeroCompleto);
-        this.usuario = usuario;
-    }
 
     @Override
     public void enviarMensaje(String mensaje) {
-        //System.out.println("Enviando SMS a " + usuario + ": " + mensaje);
-        // to do
+        // to do (implementar twilio)
     }
 }
--- notificaciones/src/main/java/com/ddsi/notificaciones/domain/ContactoWhatsapp.java
+++ notificaciones/src/main/java/com/ddsi/notificaciones/domain/ContactoWhatsapp.java
@@ -1,14 +1,8 @@
 package com.ddsi.notificaciones;
 
 public class ContactoWhatsapp extends ContactoTelefono {
-
-    public ContactoWhatsapp(String numeroCompleto) {
-        super(numeroCompleto);
-    }
-
     @Override
     public void enviarMensaje(String mensaje) {
-       // System.out.println("Enviando WHATSAPP: " + mensaje);
-        // to do
+        // to do.. (implementar whappi)
     }
 }
