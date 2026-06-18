827d2287f00626fecbd99cfb47a57de0b056ea01
Antonella Justine <antonellajus2005@gmail.com>
2026-06-18 12:13:34 -0300

    borramos excepciones..

--- notificaciones/src/main/java/com/ddsi/notificaciones/exception/ContactoInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package com.ddsi.notificaciones.exception;
-
-public class ContactoInvalidoException extends RuntimeException {
-    public ContactoInvalidoException(String mensaje) {
-        super(mensaje);
-    }
-}
