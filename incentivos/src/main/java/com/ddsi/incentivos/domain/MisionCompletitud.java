37243d0a39f6c907b4464faad4fde0f03eb51108
catalina-gc <cgonzalezcaceres@frba.utn.edu.ar>
2026-06-18 22:55:26 -0300

    Mision Completitud agregada

--- incentivos/src/main/java/com/ddsi/incentivos/domain/MisionCompletitud.java
+++ incentivos/src/main/java/com/ddsi/incentivos/domain/MisionCompletitud.java
@@ -1,5 +1,16 @@
 package com.ddsi.incentivos.domain;
 
 public class MisionCompletitud extends Mision {
-
+    @Override
+    public int getProgresoActual(Donante donante) {
+        <ArrayList>DonacionIndependienteDTO donaciones= donante.getDonaciones();
+        <ArrayList> String categorias;
+        for(int i = 0; i < donaciones.size();i++){
+            DonacionIndependienteDTO donacion = donaciones.get(i);
+            if(!categorias.contains(donacion.getCategoriaBien())){
+                categorias.add(donacion.getCategoriaBien());
+            }
+        }
+        return categorias.size();
+    }
 }
