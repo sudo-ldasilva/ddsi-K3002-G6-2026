b988fe43fa89ebf5075cac1f47f7f7de5cd2d9b0
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-06-18 22:39:13 -0300

    Implementa Categoria en el servicio de Incentivos

--- incentivos/src/main/java/com/ddsi/incentivos/domain/Categoria.java
+++ incentivos/src/main/java/com/ddsi/incentivos/domain/Categoria.java
@@ -1,5 +1,27 @@
 package com.ddsi.incentivos.domain;
 
-public class Categoria {
+import java.util.ArrayList;
 
+public class Categoria {
+    private String nombre;
+    private ArrayList<Mision> misionesACompletar;
+    private Categoria categoriaSiguiente;
+
+    public Categoria(String nombre, ArrayList<Mision> misionesACompletar, Categoria categoriaSiguiente) {
+        this.nombre = nombre;
+        this.misionesACompletar = misionesACompletar;
+        this.categoriaSiguiente = categoriaSiguiente;
+    }
+
+    public boolean puedePasarASiguienteCategoria(Donante donante) {
+        return misionesACompletar.stream().allMatch( m -> m.misionCumplida(donante));
+    }
+
+    public Mision getMision(int indice) {
+        return misionesACompletar.get(indice);
+    }
+
+    public Categoria getSiguiente() {
+        return categoriaSiguiente;
+    }
 }
