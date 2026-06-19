e5c97810503c856017c659335c882d384de93579
catalina-gc <cgonzalezcaceres@frba.utn.edu.ar>
2026-06-18 22:43:21 -0300

    Mision y MisionRacha agregados

--- incentivos/src/main/java/com/ddsi/incentivos/domain/Mision.java
+++ incentivos/src/main/java/com/ddsi/incentivos/domain/Mision.java
@@ -1,5 +1,22 @@
 package com.ddsi.incentivos.domain;
+import java.util.Date;
 
 public abstract class Mision {
+    private String nombre;
+    private int cantidadNecesaria;
+    private String unidadDeMedida;
 
+    public Insignia misionCumplida(Donante donante){
+        Mision misionActual = donante.getMisionActual();
+        if (misionActual.getProgresoActual(donante) == misionActual.cantidadNecesaria){
+            return insignia = new Insignia (new Date(),self);
+        }
+        return null;
+    }
+
+    public int getCantidadNecesaria() {
+        return cantidadNecesaria;
+    }
+
+    public abstract int getProgresoActual(Donante donante);
 }
--- incentivos/src/main/java/com/ddsi/incentivos/domain/MisionRacha.java
+++ incentivos/src/main/java/com/ddsi/incentivos/domain/MisionRacha.java
@@ -1,5 +1,32 @@
 package com.ddsi.incentivos.domain;
 
 public class MisionRacha extends Mision {
+    @Override
+    public int getProgresoActual(Donante donante) {
+        int progreso = 0;
+        ArrayList<DonacionIndependienteDTO> donaciones = donante.getDonaciones();
+        java.util.Date fecha = new Date();
+        int mesActual = fecha.getMonth();
+        for (int i = 0, i <this.getCantidadNecesaria(), i++){
+            int a = this.esteMes(donaciones, mesActual);
+            if (a == 0) {
+                return progreso;
+            }
+            progreso = +a;
+            mesActual--;
+            if (mesActual == -1) {
+                mesActual = 11;
+            }
+        }
+    }
+    int esteMes(ArrayList<DonacionIndependienteDTO> donaciones, int mes){
+        for (int i = 0, i<donaciones.size(), i++){
+            DonacionIndependienteDTO donacion = donaciones.get(i);
+            if(donacion.getFecha().getMonth()==mes) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 
 }
