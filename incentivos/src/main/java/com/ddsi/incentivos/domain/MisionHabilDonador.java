438fd7f27f71d10e4cef4b9f909aa736f63e9c55
catalina-gc <cgonzalezcaceres@frba.utn.edu.ar>
2026-06-18 23:05:07 -0300

    MisionHabilDonador agregada

--- incentivos/src/main/java/com/ddsi/incentivos/domain/MisionHabilDonador.java
+++ incentivos/src/main/java/com/ddsi/incentivos/domain/MisionHabilDonador.java
@@ -1,5 +1,16 @@
 package com.ddsi.incentivos.domain;
 
 public class MisionHabilDonador extends Mision {
-
+    @Override
+    public int getProgresoActual(Donante donante) {
+        <ArrayList>DonacionIndependienteDTO donaciones = donante.getDonaciones();
+        int max = 0;
+        for (int i = 0; i<donaciones.size(); i++){
+            DonacionIndependienteDTO donacion = donaciones.get(i);
+            if(donacion.getCantidadBienes()>max){
+                max = donacion.getCantidadBienes();
+            }
+        }
+        return max;
+    }
 }
