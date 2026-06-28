9baeb74cb24a4152c021b1f4cfa10397426eb1f5
ayevitasse <avitasse@frba.utn.edu.ar>
2026-06-28 20:11:28 -0300

    espero que funcione

--- notificaciones/src/main/java/com/ddsi/notificaciones/domain/JavaMail.java
+++ notificaciones/src/main/java/com/ddsi/notificaciones/domain/JavaMail.java
@@ -1,29 +1,29 @@
 package com.ddsi.notificaciones.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JavaMail implements EnvioEmailAdapter {
+
     private final JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
     private String mailUser;
 
-    public JavaMail(String mailUser) {
-        this.mailUser = mailUser;
+    public JavaMail(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
     @Override
     public void enviarMail(String direccion, String mensaje) {
-
-        SimpleMailMessage mail =
-                new SimpleMailMessage();
-
+        SimpleMailMessage mail = new SimpleMailMessage();
         mail.setFrom(mailUser);
         mail.setTo(direccion);
         mail.setSubject("Notificación");
         mail.setText(mensaje);
-
         mailSender.send(mail);
     }
 }
