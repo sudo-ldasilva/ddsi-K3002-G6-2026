947d789aa62bc1b3b7463aa1ce0d9815b48b19ae
annkizz <antonellajus2005@gmail.com>
2026-06-28 17:45:43 -0300

    creo la clase notificador strategy

---
+++ notificaciones/src/main/java/com/ddsi/notificaciones/domain/NotificadorStrategy.java
@@ -0,0 +1,7 @@
+package com.ddsi.notificaciones.domain;
+
+import com.ddsi.notificaciones.notificacion;
+
+public interface NotificadorStrategy {
+    public void enviarMensaje(Notificacion notificacion);
+}
