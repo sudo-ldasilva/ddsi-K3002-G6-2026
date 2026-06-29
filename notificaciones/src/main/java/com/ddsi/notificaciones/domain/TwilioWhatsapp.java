a818b603051fb106c05ab55c3aa99bd6c9d56d0a
ayevitasse <avitasse@frba.utn.edu.ar>
2026-06-28 22:25:37 -0300

    se envian whatsapps ya

--- notificaciones/src/main/java/com/ddsi/notificaciones/domain/TwilioWhatsapp.java
+++ notificaciones/src/main/java/com/ddsi/notificaciones/domain/TwilioWhatsapp.java
@@ -1,12 +1,35 @@
 package com.ddsi.notificaciones.domain;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwilioWhatsapp implements EnvioWhatsAppAdapter {
 
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth.token}")
+    private String authToken;
+
+    @Value("${twilio.whatsapp.from}")
+    private String from;
+
+    @PostConstruct
+    public void init() {
+        Twilio.init(accountSid, authToken);
+    }
+
     @Override
     public void enviarWsp(String direccion, String mensaje) {
-        // to do.. (implementar whappi)
+        Message.creator(
+                new PhoneNumber("whatsapp:" + direccion),
+                new PhoneNumber(from),
+                mensaje
+        ).create();
     }
 }
