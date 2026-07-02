26d78514a3097f98c16240776c50705cfab07fc0
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-07-02 19:56:02 -0300

    Agrega ejemplo de notificación en donaciones

--- donaciones/src/main/java/com/ddsi/donaciones/DonacionesApplication.java
+++ donaciones/src/main/java/com/ddsi/donaciones/DonacionesApplication.java
@@ -1,19 +1,29 @@
 package com.ddsi.donaciones;
 
-import com.ddsi.donaciones.service.NotificacionDispatcherService;
+import java.util.ArrayList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.ddsi.donaciones.domain.Contacto;
+import com.ddsi.donaciones.service.NotificacionDispatcherService;
 
 @SpringBootApplication
 @EnableScheduling
 public class DonacionesApplication {
 
+    private void molestarAAnto() {
+        NotificacionDispatcherService notif = new NotificacionDispatcherService();
+        ArrayList<Contacto> cs = new ArrayList<>();
+        cs.add(new Contacto("+5491168603322", "SMS"));
+        notif.notificar(
+            cs,
+            "Hola"
+        );
+    }
+
 	public static void main(String[] args) {
 		System.out.println("Hola, mundo!");
 		SpringApplication.run(DonacionesApplication.class, args);
-		NotificacionDispatcherService notificacionDispatcherService = new NotificacionDispatcherService();
-		notificacionDispatcherService.notificar();
 	}
-
 }
