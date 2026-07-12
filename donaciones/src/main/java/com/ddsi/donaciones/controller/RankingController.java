2e2cd822131cd85b4090efe6dccaae50637c4c3b
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-07-12 16:13:49 -0300

    Separa el concepto de ranking del servicio de incentivos y donaciones

--- donaciones/src/main/java/com/ddsi/donaciones/controller/RankingController.java
+++ donaciones/src/main/java/com/ddsi/donaciones/controller/RankingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/rankings")
+@RequestMapping("/rankingNecesidades")
 public class RankingController {
 
     @GetMapping("/{uuidDonacionIndependiente}")
